d2dd826ed8a45da327dc4a15ef062173bf32a642
solk2 <solk@solk.org.ua>
2014-06-08 18:12:34 +0300

    Fixed wrong acdb_id for Camera. Fixed model name.

--- audio/platform/customplatform.c
+++ audio/platform/customplatform.c
@@ -76,7 +76,7 @@ void custom_init_data(){
     change_acdb_data(SND_DEVICE_IN_VOICE_SPEAKER_DMIC, 11);
     change_acdb_data(SND_DEVICE_IN_SPEAKER_DMIC, 11);
     change_acdb_data(SND_DEVICE_IN_VOICE_REC_MIC, 53);
-    change_acdb_data(SND_DEVICE_IN_CAMCORDER_MIC, 41);
+    change_acdb_data(SND_DEVICE_IN_CAMCORDER_MIC, 56);
     change_acdb_data(SND_DEVICE_OUT_USB_HEADSET, 98);
     change_acdb_data(SND_DEVICE_OUT_BT_SCO, 48);
     change_acdb_data(SND_DEVICE_OUT_BT_SCO_WB, 50);
