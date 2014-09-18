b4122ac8797d2b08b75b7961088115cfff001e45
solk2 <solk@solk.org.ua>
2014-09-18 23:52:54 +0300

    KS01LTE: Update boot.img offsets. How did it boot in first place?:)
    Change-Id: Ib3792ca6b0003d264fc21b022d0ef19718570762

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_KERNEL_SOURCE := kernel/samsung/ks01lte
 TARGET_KERNEL_CONFIG := msm8974_sec_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := msm8974_sec_ks01_eur_defconfig
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --second_offset 0x00f00000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
