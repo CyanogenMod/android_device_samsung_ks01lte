cb50adc511c4ee0b5df7e5e994feac3827a503a6
solk2 <solk@solk.org.ua>
2014-10-09 15:38:44 +0300

    ks01lte: Fix camera properties permissions.
    Change-Id: I8bc73093f254f75de86dddb8fb1262a7a5c0ef2f

--- include/device_perms.h
+++ include/device_perms.h
@@ -19,6 +19,7 @@
 
 #define PROPERTY_PERMS_APPEND \
     { "persist.audio.",   AID_SYSTEM,   0 }, \
-    { "persist.sys.camera.", AID_MEDIA, 0 }, \
+    { "persist.camera.", AID_MEDIA, 0 }, \
+    { "service.camera.", AID_MEDIA, 0 }, \
     { "wlan.hdcp2.", AID_MEDIA, 0 },
 #endif /* DEVICE_PERMS_H */
