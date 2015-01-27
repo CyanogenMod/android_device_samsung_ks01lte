67d14bc96fc09e6b0b73e19bced9b6637068a586
Vitalii Kulikov <solk@solk.org.ua>
2015-01-27 20:39:41 +0200

    ks01lte: Set selinux enforcing.
    Change-Id: I5ddb8c95d8fb51457486d510968cc8a3fe39a515

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_KERNEL_SOURCE := kernel/samsung/ks01lte
 TARGET_KERNEL_CONFIG := cyanogenmod_ks01lte_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --second_offset 0x00f00000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
