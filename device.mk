c271621dc08b70f1685d03c5f9333323a5cf33ff
Vitalii Kulikov <solk@solk.org.ua>
2015-02-20 01:50:15 +0200

    ks01lte: Kang gps blobs from oppo.
    Change-Id: I76152d6d38167f4675a2c7adfc990511008b4000

--- device.mk
+++ device.mk
@@ -70,7 +70,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/gps.conf:/system/etc/gps.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:/system/etc/sap.conf \
-    $(LOCAL_PATH)/gps/etc/flp.conf:/system/etc/flp.conf
+    $(LOCAL_PATH)/gps/etc/flp.conf:/system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/quipc.conf:/system/etc/quipc.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:/system/etc/izat.conf
 
 # Input device
 PRODUCT_COPY_FILES += \
