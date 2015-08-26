716e08c572054d5fc7e30851f9f2b47478394d97
Vitalii Kulikov <solk@solk.org.ua>
2015-08-26 15:41:42 +0200

    ks01lte: Switch back to opensource libgps.utils.so ... and update gps.conf
    Change-Id: I8b1c586073021d66431a2d8357441fd5b2f92d53

--- gps/utils/sec_gps.c
+++ gps/utils/sec_gps.c
@@ -1,9 +1,13 @@
-int sec_gps_conf(void)
+/* secgps.conf parameters */
+typedef struct sec_gps_cfg_s
 {
-    return 0;
-}
+    char      OPERATION_MODE[50];
+} sec_gps_cfg_s_type;
 
-int Sec_Configuration(void)
+typedef struct Sec_Configuration_s
 {
-    return 0;
-}
+    char      STUB[50];
+} Sec_Configuration_s_type;
+
+sec_gps_cfg_s_type sec_gps_conf = { .OPERATION_MODE = "MSBASED" };
+Sec_Configuration_s_type Sec_Configuration = { .STUB = "" };
